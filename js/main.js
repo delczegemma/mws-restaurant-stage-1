66e21539915ffb372672258c722f2a004621fda3
delczegemma <delczegemma@gmail.com>
2018-08-17 14:59:02 +0200

    fix: Order headings hierarchically

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute("alt", DBHelper.altForRestaurant(restaurant));
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
